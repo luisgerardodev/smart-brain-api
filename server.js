6ae31b003ed49c029d7ccd2f9adb9704deb1f947
Luima <yaminolight@gmail.com>
2020-01-25 12:37:22 -0400

    added db connection

--- server.js
+++ server.js
@@ -21,6 +21,14 @@ const app = express();
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.get('/', (req, res) => { res.json('It is working!s') });
 app.post('/signin', (req, res) => { signin.handleSignIn(req, res, db, bcrypt) });
